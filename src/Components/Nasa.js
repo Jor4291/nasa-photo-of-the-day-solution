5ca36830383b3285e811576dffff3d1b7cd41b94
James Robertson <Jor4291@gmail.com>
2021-12-09 21:59:23 -0600

    After studying for hours I just dont know what Im not getting. Its just not clicking for me and I am guessing at every line of code.

--- src/Components/Nasa.js
+++ src/Components/Nasa.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Nasa = (props) => {
   return (
     <div>
-      <h1>NASA FUN</h1>
+      <h1>NASA's Picture of the Day!</h1>
       <img src={props.nasaData.url} alt="Nasa APOD" />
       <p>{props.nasaData.date}</p>
       <p>{props.nasaData.explanation}</p>
